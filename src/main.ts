228e074eeff1ab0701f03c0d9663c79d89c6565a
todd.kim <sksms20000@gmail.com>
2024-07-19 14:29:45 +0900

    handle base exception.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost, BaseExceptionFilter } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    const { httpAdapter } = app.get(HttpAdapterHost);
+    app.useGlobalFilters(new BaseExceptionFilter(httpAdapter));
     await app.listen(3000);
 }
 bootstrap();
